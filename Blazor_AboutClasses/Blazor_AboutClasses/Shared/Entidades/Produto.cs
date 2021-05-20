49a1a433bb902309e24eada2ba7581c79eed7b85
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-05-20 13:47:18 -0300

    classes, repetições e condições

---
+++ Blazor_AboutClasses/Blazor_AboutClasses/Shared/Entidades/Produto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace Blazor_AboutClasses.Shared.Entidades
+{
+   public class Produto
+    {
+        public string Nome { get; set; }
+        public DateTime DataDoCadastro { get; set; }
+    }
+}
