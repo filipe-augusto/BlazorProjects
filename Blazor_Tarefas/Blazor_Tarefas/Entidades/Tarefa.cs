8ec0d5b0e330437ba6369c3dcbc1d8813aa8b160
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-05-21 16:28:15 -0300

    .

---
+++ Blazor_Tarefas/Blazor_Tarefas/Entidades/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Blazor_Tarefas.Entidades
+{
+   
+        public class Tarefa
+        {
+            public Guid Id { get; set; }
+            public string Descricao { get; set; }
+            public bool Concluida { get; set; }
+            public DateTime DataCricao { get; set; }
+        
+    }
+}
