17fdd7598721b35eb12b190767d98aff48db8e7a
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-05-24 19:51:04 -0300

    ...
    ....

--- Blazor_Tarefas/Blazor_Tarefas/Program.cs
+++ Blazor_Tarefas/Blazor_Tarefas/Program.cs
@@ -1,25 +1,16 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace Blazor_Tarefas
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
     }
 }
