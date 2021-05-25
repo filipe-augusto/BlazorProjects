0746ce0a62b8a3ae10f4902687034538e94e6b50
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-05-25 09:29:31 -0300

    invocando js

---
+++ Blazor_Tarefas/Blazor_Tarefas/wwwroot/JSFunction.js
@@ -0,0 +1,16 @@
+﻿window.MostraAlerta = (message) =>
+    alert(message);
+
+function  getTotalTarefas() {
+    DotNet.invokeMethodAsync("Blazor_Tarefas", "ObterTotalTarefas")
+        .then(resultado => {
+            alert("total de tarefas : " + resultado)
+        })
+}
+
+function getTotalTarefasInstancia(dotnet) {
+   return  dotnet.invokeMethodAsync("ObterTotalTarefasInstancia")
+        .then(resultado => {
+            alert("total de tarefas : " + resultado)
+        })
+}
