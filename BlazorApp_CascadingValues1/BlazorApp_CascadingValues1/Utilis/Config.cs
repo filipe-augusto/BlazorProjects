20992382de586a9b9d5810e3ebbe420b8f72fdf1
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-05-25 14:15:09 -0300

    paramtros arbritarios

---
+++ BlazorApp_CascadingValues1/BlazorApp_CascadingValues1/Utilis/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp_CascadingValues1.Utilis
+{
+    public class Config
+    {
+        public string Cor { get; set; } = "cyan";
+        public string Tamanho { get; set; } = "60px";
+    }
+}
