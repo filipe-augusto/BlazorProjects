3d785d223f5a532132a0a69d7f3e04f53c7967be
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-05-28 08:13:19 -0300

    Validator and formulario

---
+++ BlazorApp_Forms1/BlazorApp_Forms1/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp_Forms1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Range(18,80,ErrorMessage ="A idade deve estar entre 18 e 80")]
+        public int Idade { get; set; }
+    }
+}
