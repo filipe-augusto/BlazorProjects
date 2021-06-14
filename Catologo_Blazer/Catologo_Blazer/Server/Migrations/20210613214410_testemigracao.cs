d811d534b46ff93609fcbea7a1f6388a1d824eb2
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2021-06-13 21:12:26 -0300

    ...

---
+++ Catologo_Blazer/Catologo_Blazer/Server/Migrations/20210613214410_testemigracao.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catologo_Blazer.Server.Migrations
+{
+    public partial class testemigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
