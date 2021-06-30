be88b41d426285e9062d0f10435bd0bc5de7b6bc
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-06-30 20:27:48 -0300

    revisao primeiro programa

---
+++ Revisao_CursoMacoratti/Revisao_CursoMacoratti/Client/Classes/Calculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revisao_CursoMacoratti.Client.Classes
+{
+    public class Calculo
+    {
+        public static int Somar(int i, int y) => i + y;
+        public static int Multiplicar(int i, int y) => i * y;
+        public static int Substrair(int i, int y) => i - y;
+    }
+}
---
+++ Revisao_CursoMacoratti/Revisao_CursoMacoratti/Shared/Entidades/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Revisao_CursoMacoratti.Shared.Entidades
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
