cd22c09510a923e6cf58c803272b0fee32493edc
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-07-20 10:39:11 -0300

    revisando o curso

--- Blazor_Forms1/Blazor_Forms1/Models/Usuario.cs
+++ Blazor_Forms1/Blazor_Forms1/Models/Usuario.cs
@@ -1,10 +1,18 @@
-﻿namespace Blazor_Forms1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Forms1.Models
 {
     public class Usuario
     {
+
         public int Id { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado.")]
+        [StringLength(10,ErrorMessage ="O nome não pode exceder 10 caracteres")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O e-mail deve ser informado.")]
+        [EmailAddress( ErrorMessage="E-mail no formato invalido.")]
         public string Email { get; set; }
+        [Range(18,80, ErrorMessage ="A idade deve esar entre 18 e 80")]
         public int Idade { get; set; }
     }
 }
---
+++ Tarefas_Revisao/Tarefas_Revisao/Utils/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarefas_Revisao.Utils
+{
+    public class Config
+    {
+        public string CorDeFundo { get; set; } = "cyan";
+        public string TamanhoFonte { get; set; } = "60px";
+    }
+}
