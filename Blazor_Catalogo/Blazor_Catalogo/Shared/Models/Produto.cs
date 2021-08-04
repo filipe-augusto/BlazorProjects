bd5b329ecdf23655f16e556dc227029035786ef5
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-08-04 16:05:02 -0300

    projeto - implementando segurança

---
+++ Blazor_Catalogo/Blazor_Catalogo/Shared/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Catalogo.Shared.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do produto")]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição do produto")]
+        [MaxLength(200)]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        public decimal Preco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
---
+++ Blazor_Catalogo/Blazor_Catalogo/Shared/Models/UserToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Catalogo.Shared.Models
+{
+    public class UserToken
+    {
+
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public string Message { get; set; }
+    }
+}
