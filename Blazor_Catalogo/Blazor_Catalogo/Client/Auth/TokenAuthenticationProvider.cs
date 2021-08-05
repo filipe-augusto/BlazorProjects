8a51117234c6a05347f524788c47b570094456b1
Filipe Augusto <67299107+filipe-augusto@users.noreply.github.com>
2021-08-05 07:14:46 -0300

    aaaaaaaaaa

---
+++ Blazor_Catalogo/Blazor_Catalogo/Client/Auth/TokenAuthenticationProvider.cs
@@ -0,0 +1,30 @@
+﻿using Blazor_Catalogo.Client.Utils;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Blazor_Catalogo.Client.Auth
+{
+    public class TokenAuthenticationProvider : AuthenticationStateProvider
+    {
+
+        private readonly IJSRuntime js;
+        private readonly HttpClient http;
+        public static readonly string tokenKey = "tokenKey";
+
+        public TokenAuthenticationProvider(IJSRuntime iJSRuntime, HttpClient httpClient)
+        {
+            js = iJSRuntime;
+            http = httpClient;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await js.GetFromLocalStorage(tokenKey);
+        }
+    }
+}
